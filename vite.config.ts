da0c6f3390244c2cc110a3bb2cc7c0271316f5b5
lkwoung88 <lkwoung88@gmail.com>
2024-08-19 22:16:36 +0900

    프로젝트 세팅 - react query - eslint - vite config - env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    server: {
+        host: true,
+        port: 9000,
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+                rewrite: path => path.replace(/^\/api/, ''),
+            },
+        },
+    },
+    plugins: [
+        react()
+    ],
+    resolve: {
+        alias: {
+            '@': '/src'
+        }
+    }
 })
