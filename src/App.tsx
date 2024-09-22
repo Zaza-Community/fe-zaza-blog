299bd4833ee913eb20203d854ff1fb589b652018
chlangus <angusangus11@gmail.com>
2024-09-22 16:45:23 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import './App.css';
-import { Login } from './component/login/Login.tsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import Carousel from './component/Carousel';
+import Nav from './component/Nav';
 
 function App() {
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <Login />
-    </QueryClientProvider>
+    <>
+      <Nav />
+      <Carousel />
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,43 @@
 import { StrictMode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Community from './pages/Community.tsx';
+import Tech from './pages/Tech.tsx';
+import Nav from './component/Nav.tsx';
+import News from './pages/News.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    element: <Nav />,
+    children: [
+      {
+        path: '/news',
+        element: <News />,
+      },
+      {
+        path: '/tech',
+        element: <Tech />,
+      },
+      {
+        path: '/community',
+        element: <Community />,
+      },
+    ],
+  },
+]);
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </StrictMode>,
 );
